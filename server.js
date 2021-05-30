824d55e507cb58d61a9d6882f21ae2135a9f2c9e
japankid-code <japankid.jake@gmail.com>
2021-05-30 09:32:06 -0500

    chat events now emit and broadcast via socket

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
 
 server.listen(3000, () => {
